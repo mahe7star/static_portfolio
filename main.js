946425f51c92959531181391fd89f46fa93c53e5
mahe7star <mp1995singh@gmail.com>
2019-09-01 15:25:27 +0530

    initial ommit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/* ------google maps---*/
+function initMap() {
+  var uluru = {lat: 28.607832, lng: 77.064299};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
